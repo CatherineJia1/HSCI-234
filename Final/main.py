ff18611abd1e4ff2ab13b8797b87526949cab90d
CatherineJia1 <123411968+CatherineJia1@users.noreply.github.com>
2023-04-08 22:05:10 -0700

    Update main.py

--- Final/main.py
+++ Final/main.py
@@ -145,8 +145,8 @@ def draw():
             #programState = "Scene2-2"
     elif(programState == "Scene2-2"):
         Scene2.draw() 
-    if(programState != "Scene1-1" and programState != "Scene1-0"and programState != "Scene1-2" and programState != "Scene2-1"):
-        sound.play()
+    #if(programState != "Scene1-1" and programState != "Scene1-0"and programState != "Scene1-2" and programState != "Scene2-1"):
+        #sound.play()
     elif(programState == "Scene1-4"):
         SceneRoom1.draw()
     elif(programState == "Scene3-0"):
@@ -240,6 +240,7 @@ def keyReleased(event):
     pass
 
 def mousePressed(event):
+    sound.play()
     global b,c,windowState,putChair, gameState, RapunzelState, tableState,starState
     if(p5.mouseY>330 and p5.mouseY<370 and b==1):
         c=c+1
