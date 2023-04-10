1169cf69c345ab7d0d2f66177ce0e23f22ecb4ba
CatherineJia1 <123411968+CatherineJia1@users.noreply.github.com>
2023-04-09 17:06:13 -0700

    Update main.py

--- Final/main.py
+++ Final/main.py
@@ -111,6 +111,7 @@ def setup():
     p5.createCanvas(600, 400) 
     global sound
     sound = p5.loadSound('Alan Menken - Waiting for the Lights.mp3')
+    Sound.loop();
     print('finish setup') 
   
     
