2cc3e890675ab8dc5163e32dabb28289f83a4254
CatherineJia1 <123411968+CatherineJia1@users.noreply.github.com>
2023-12-03 22:49:49 -0800

    Update main.py

--- class06hw/main.py
+++ class06hw/main.py
@@ -1,88 +1,45 @@
-#press 1, 2, 3 to change to state 1, 2, 3
-#automatically changed to state 2 after stayed in state 1 / state 3 for 3 second 
-# press to change from 2 to 3
-
 import js
-p5 = js.window
-program_time = p5.millis()
-program_state = 'STATE1'
-e1=150
-e2=170
-e3=190
-x=150
-change = 1
-speed = 1
 
+class Bullet:  
+  def __init__(self, x = 150, y = 250, speed = 2):
+    self.x = x  
+    self.y = y
+    self.speed = speed
+
+  def draw(self):
+    p5.push()
+    p5.translate(self.x, self.y)
+    p5.fill(255, 0, 127)
+    p5.rect(0, 0, 40, 80)
+    p5.pop()
+
+   def update(self):
+       distance = p5.dist(self.x, self.y, p5.mouseX, p5.mouseY)
+       if(distance < 10):
+            self.y = self.y - self.speed
+           
+
+bullet = Bullet(150, 0, 2)  
 
 def setup():
-    p5.createCanvas(300, 300)  
-    print('finished setup..')
-
+  p5.createCanvas(300, 300)  
+  p5.rectMode(p5.CENTER)
+  p5.imageMode(p5.CENTER)
+  p5.textFont(font)
+  p5.textSize(18)
+  p5.text(str(p5.mouseX) + ", " + str(p5.mouseY), 10, 15)
 
 def draw():
-    global program_time, program_state,e1,e2,e3,x,change,speed
-    p5.background(255)   
-    p5.fill(0)
-    p5.text('program_state = ' + program_state, 10, 70)
-    p5.text(str(p5.mouseX) + ", " + str(p5.mouseY), 10, 15)
-    p5.text('milliseconds: ' + str(program_time), 10, 35)
-    sec = int(program_time / 1000)
-    p5.text('seconds: ' + str(sec), 10, 55)
-    p5.noFill()
+  p5.background(0) 
+  bullet.draw()
 
-    if(p5.millis() > program_time + 3000):
-        program_state = 'STATE2'
-        program_time = p5.millis()
-       
-    if(program_state == 'STATE2'): #second State, changing size of circle
-        e1 = e1-change
-        e2 = e2-change
-        e3 = e3-change
-        if ( e1 <= 0 or e1>x or e1>300-x):
-            change =-change
-        draw_circle()
-        
-    if(program_state == 'STATE3'):#third State, move the circle
-        move_circle()
-        draw_circle()
-        
-    if(program_state == 'STATE1'):
-        p5.rect(75,75,150,150)
-        
-def move_circle(): #how to move circle
-    global x,e1,e2,e3,speed
-    x = x+speed
-    if (x>e3 and x<p5.width-e3):
-        speed = speed
-    else:
-        speed = -speed
-
-
-def draw_circle(): #draw circle
-    global x,e1,e2,e3
-    p5.ellipse(x,150,e1,e1)
-    p5.ellipse(x,150,e2,e2)
-    p5.ellipse(x,150,e3,e3) 
-    
 def keyPressed(event):#keys 1,2,3 to change state
-    global program_state 
-    if(str(p5.key) == '1'):
-        program_state = 'STATE1'
-    if(str(p5.key) == '2'):
-        program_state = 'STATE2'
-    if(str(p5.key) == '3'):
-        program_state = 'STATE3'
-    print('change program_state to ' + program_state)
+    pass
 def keyReleased(event):
     pass
 
 def mousePressed(event): #press to change state
-    global program_time
-    global program_state
-    program_state = 'STATE3'
-    print('change program_state to ' + program_state)
-    program_time = p5.millis()
-
+    pass
 
 def mouseReleased(event):
-    print('mouseReleased..') 
+    pass
