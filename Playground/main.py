e74381fbd6e296f21f8f80a864b6301d72331aef
CatherineJia1 <123411968+CatherineJia1@users.noreply.github.com>
2024-07-19 09:14:43 -0700

    Update main.py

--- Playground/main.py
+++ Playground/main.py
@@ -1,100 +1,84 @@
+import js as p5
+from js import document
+import math
+
+img1 = p5.loadImage('Frame 52.png')
+img2 = p5.loadImage('Group 31.png')
+img3 = p5.loadImage('Group 4.png')
+
+position = []
+
+running = True
+last_record_time = 0
+record_interval = 50  # 10ms
+pop_interval = 3000  # 300ms
 
 
-import js
-p5 = js.window
-speed=0.3
-
-
-
-class Player:  
-    def __init__(self, x = 100, y = 150, state = "Up"):
-        self.x = x  # initialize attribute x 
-        self.y = y  # initialize attribute y 
-        self.state = state# initialize attribute state
-
-    def set_player(self, x, y, state):  #reset position
-        self.x = x  
-        self.y = y
-        self.state = state
-
-    def move_player(self, distance_x, distance_y):#move player
-        self.x += distance_x
-        self.y += distance_y
-
-    def update(self,state): #chang state
-        self.state=state
-
-    # method to draw a Point object:
-    def draw(self):
-        if(p5.dist(player_1.x, player_1.y,player_2.x,player_2.y)<60):  #if players are close to each other turn red
-            p5.fill(256,0,0)
-            if(p5.dist(player_1.x, player_1.y,player_2.x,player_2.y)<50):#if players touched each other, reset
-                player_1.set_player(100,150,"Up")
-                player_2.set_player(200,150,"Up")
-        else:
-            p5.noFill()
-        p5.push()
-        
-        p5.translate(self.x, self.y)
-        if(self.state == 'Right'): #change direction of player according to state
-            p5.rotate(p5.radians(360/4))
-        if(self.state == 'Left'):  
-            p5.rotate(p5.radians(360/4*3))  
-        if(self.state == 'Down'):
-            p5.rotate(p5.radians(180))
-        p5.ellipse(0, 0, 50, 50)
-        p5.ellipse(-10,-5,10,10)
-        p5.ellipse(10,-5,10,10)
-        p5.arc(0,15,10,10,1,-4)        
-        p5.pop()
-        
-player_1 = Player()
-player_2 = Player(200,150,"Up")
 
 def setup():
-    p5.createCanvas(800, 500) 
-    print('finished setup') 
-    
-    
+  p5.createCanvas(900, 600)
+  # change mode to draw rectangles from center:
+  p5.rectMode(p5.CENTER)
+  # change mode to draw images from center:
+  p5.imageMode(p5.CENTER)
+  # change stroke cap to square:
+  p5.strokeCap(p5.SQUARE)
+
 def draw():
-    global speed
-    p5.background(255)
-    player_1.draw() 
-    player_2.draw()
-    if(player_2.x > 250 or player_2.x < 50): #make player2 move automaticlaly
-        speed = -speed
-    player_2.x += speed
+
+
+  
+  global position, last_record_time, record_interval,pop_interval
+  p5.background(255)
+  p5.image(img1,450,300,900,600)
+  p5.noStroke()
+
+  current_time = p5.millis()
+  if current_time - last_record_time >= record_interval:
+    getMousePosition()
+    last_record_time = current_time
+
+  if len(position) >60:
+    position.pop(0)
+
+  for pos in position:
+    p5.fill(255,pos[0]/3,pos[1]/3)
+    p5.ellipse(math.ceil(pos[0]/75)*75, math.ceil(pos[1]/50)*50, 75,75)
+    p5.fill(255,pos[0]/3,pos[1]/3,80)
+    p5.ellipse(math.ceil(pos[0]/75)*75, math.ceil(pos[1]/50)*50, 100,100)
+    p5.fill(255,pos[0]/3,pos[1]/3,50)
+    p5.ellipse(math.ceil(pos[0]/75)*75, math.ceil(pos[1]/50)*50, 150,150)
+ 
+
+  p5.image(img2,58,516,80,101)
+  p5.image(img3,41,38,46,46)
+  # # assign content of "data" div on index.html page to variable:
+  # # split data_string by comma, making a list:
 
 
 
 
-    
-def keyPressed(event): #when key is pressed cmove the player and change the state. 
-    if(p5.keyCode == p5.RIGHT_ARROW):
-        print('move point 10 pixels to the right..')
-        player_1.move_player( 10, 0)
-        player_1.update("Right")
-        
-    if(p5.keyCode == p5.LEFT_ARROW):
-        print('move point 10 pixels to the right..')
-        player_1.move_player( -10, 0)
-        player_1.update("Left")
-    if(p5.keyCode == p5.UP_ARROW):
-        print('move point 10 pixels to the right..')
-        player_1.move_player( 0, -10)
-        player_1.update("Up")
-    if(p5.keyCode == p5.DOWN_ARROW):
-        print('move point 10 pixels to the right..')
-        player_1.move_player( 0, 10)
-        player_1.update("Down")
-        
-def keyReleased(event):  
-    print('keyReleased.. ' + str(p5.key))
 
 
-def mousePressed(event):  
-    print('mousePressed..')
+
+  # p5.ellipse(0, 0, value_x/20, value_y/20)
 
 
-def mouseReleased(event):  
-    print('mouseReleased..')
+def getMousePosition():
+  global position
+  x = p5.mouseX
+  y = p5.mouseY
+  position.append((x,y))
+
+def  polygon(x, y, radius, npoints):
+  angle = p5.PI*2 / npoints
+  p5.beginShape()
+  i = 0
+  while(i<p5.PI*2):
+   
+    sx = x + p5.cos(i) * radius
+    sy = y + p5.sin(i) * radius
+    p5. vertex(sx, sy)
+    i += angle
+  
+  p5.endShape(p5.CLOSE)
